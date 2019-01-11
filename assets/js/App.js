3bb5e59a45148d431c7fc31254a462866e864a43
wagner <wagner@bradigital.com.br>
2019-01-11 00:50:01 -0200

    Adicionada a listagem de produtos

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+
 import Header from './components/header';
+import Main from './pages/main'
 
 const App = () => (
         <div className="App">
             <Header/>
+            <Main/>
         </div>
     );
 
